462770ad48611d383877e17cf0720c72c0264a2e
pmolbor054 <pmolbor054@gmail.com>
2023-10-05 10:41:57 +0200

    Fix: Nombre de archivo LICENSE

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,7 @@
- <?php
-// Autor: Pedro P Molina <pmolbor054@g.educaand.es>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+<?php
+// Autor: Pedro Molina <pedro@uco.es>
+require('HolaMundo.php');
 
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
+print new HolaMundo($nombre);
